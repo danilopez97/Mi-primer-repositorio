8f00fcaf0abcfe1f40ad83bf0d53c927293a300d
danilopez97 <estu97dani@gmail.com>
2018-09-03 11:15:52 -0600

    Esta creado el sitio admin,y creado el modelo

---
+++ blog/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import publicacion
+
+admin.site.register(publicacion)
+# Register your models here.
---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from django.utils import timezone
+
+class publicacion(models.Model):
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    fecha_creacion = models.DateTimeField(
+            default=timezone.now)
+    fecha_publicacion = models.DateTimeField(
+            blank=True, null=True)
+
+#procedimiento almacenado pero en funcion
+def publicar(self):
+        self.fecha_publicacion = timezone.now()
+        self.save()
+
+#sirve para definir cual de todos los campos muestra en los resumenes.
+def __str__(self):
+        return self.titulo      #mostar el tituo solo el campo seleccioado
+# Create your models here.
