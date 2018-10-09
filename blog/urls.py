c9ba1d6907bec63a95a367de49b243a33c92281e
danilopez97 <estu97dani@gmail.com>
2018-10-08 19:52:10 -0600

    se agrego la opcion de ver el detalle de la publicacion

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 url(r'^$', views.listado),
-#url(r'^post/(?P<pk>[0-9]+)/$', views.detalle, name='detalle'),
+url(r'^post/(?P<pk>[0-9]+)/$', views.detalle, name='detalle'),
 #url(r'^post/new/$', views.post_new, name='post_new'),
 #url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 
--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,33 @@ def listado(request):
         posts=publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
         return render(request, 'blog/listar.html', {'posts':posts})
 # Create your views here.
+
+def detalle(request, pk):
+        post = get_object_or_404(publicacion, pk=pk)
+        return render(request, 'blog/detalle.html', {'post': post})
+
+def post_new(request):
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.fecha_publicacion = timezone.now()
+                post.save()
+                return redirect('detalle', pk=post.pk)
+        else:
+            form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+        post = get_object_or_404(publicacion, pk=pk)
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.save()
+                return redirect('detalle', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})
