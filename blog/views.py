dbb2e490768f700a317e7c0e52e53054a8b56091
danilopez97 <estu97dani@gmail.com>
2018-10-21 00:37:23 -0600

    muestra el listado de articulos que estan como borrador

--- blog/views.py
+++ blog/views.py
@@ -20,7 +20,7 @@ def post_new(request):
             if form.is_valid():
                 post = form.save(commit=False)
                 post.autor = request.user
-                post.fecha_publicacion = timezone.now()
+
                 post.save()
                 return redirect('detalle', pk=post.pk)
         else:
@@ -39,3 +39,7 @@ def post_edit(request, pk):
         else:
             form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = publicacion.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_creacion')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
