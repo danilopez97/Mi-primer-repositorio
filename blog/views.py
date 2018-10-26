56dec424e94b63277b870c1118c67caea08440bb
danilopez97 <estu97dani@gmail.com>
2018-10-26 10:06:25 -0600

    se agrego la parte de autenticacion login

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,9 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView
 
 def listado(request):
         posts=publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
@@ -13,6 +16,7 @@ def detalle(request, pk):
         post = get_object_or_404(publicacion, pk=pk)
         return render(request, 'blog/detalle.html', {'post': post})
 
+@login_required
 def post_new(request):
         if request.method == "POST":
             form = PostForm(request.POST)
@@ -25,6 +29,7 @@ def post_new(request):
             form = PostForm()
         return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(publicacion, pk=pk)
     if request.method == "POST":
@@ -38,16 +43,17 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_draft_list(request):
     posts = publicacion.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_creacion')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
-
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(publicacion, pk=pk)
     post.publish()
     return redirect('detalle', pk=pk)
 
-
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(publicacion, pk=pk)
     post.delete()
